63acc12fc1e741fbe0a28e9edec2d608e254dee3
Anton Baranskyi <antonbaranskij12@gmail.com>
2023-05-24 18:12:14 +0300

    Init project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+  return <div className="App"></div>;
 }
 
 export default App;
+
+// const data = [
+//   {
+//     id: 1,
+//     name: "Iphone",
+//     imageUrl:
+//       "https://github.com/utimur/online-store-full-course/blob/master/server/static/e1646781-9115-417d-886d-976c2749a023.jpg?raw=true",
+//     RAM: "5гб",
+//     memory: "256гб",
+//     camera: 12,
+//     price: 10000,
+//     stars: 5,
+//   },
+//   {
+//     id: 1,
+//     name: "Iphone",
+//     imageUrl:
+//       "https://github.com/utimur/online-store-full-course/blob/master/server/static/e1646781-9115-417d-886d-976c2749a023.jpg?raw=true",
+//     RAM: "5гб",
+//     memory: "256гб",
+//     camera: 12,
+//     price: 10000,
+//     stars: 5,
+//   },
+//   {
+//     id: 2,
+//     name: "Iphone",
+//     imageUrl:
+//       "https://github.com/utimur/online-store-full-course/blob/master/server/static/e1646781-9115-417d-886d-976c2749a023.jpg?raw=true",
+//     RAM: "5гб",
+//     memory: "256гб",
+//     camera: 12,
+//     price: 10000,
+//     stars: 5,
+//   },
+//   {
+//     id: 3,
+//     name: "Iphone",
+//     imageUrl:
+//       "https://github.com/utimur/online-store-full-course/blob/master/server/static/e1646781-9115-417d-886d-976c2749a023.jpg?raw=true",
+//     RAM: "5гб",
+//     memory: "256гб",
+//     camera: 12,
+//     price: 10000,
+//     stars: 5,
+//   },
+//   {
+//     id: 4,
+//     name: "Iphone",
+//     imageUrl:
+//       "https://github.com/utimur/online-store-full-course/blob/master/server/static/e1646781-9115-417d-886d-976c2749a023.jpg?raw=true",
+//     RAM: "5гб",
+//     memory: "256гб",
+//     camera: 12,
+//     price: 10000,
+//     stars: 5,
+//   },
+// ];
+
+// const obj = JSON.stringify(data);
+// console.log(obj);
